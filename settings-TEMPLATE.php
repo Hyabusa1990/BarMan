20bf14160bfb6bd6c832549df9872f09c9f90cd3
Gero Gras <gero@grasfamily.de>
2020-05-07 09:11:44 +0200

    Add .gitignore + SettingsTemplate

---
+++ settings-TEMPLATE.php
@@ -0,0 +1,27 @@
+<?php
+    if (!defined("WERDICHLEGALGERUFEN")) {
+        echo ("YOU CANNOT DISPLAY THIS FILE DIRECTLY");
+        exit();
+    }
+
+    /*
+    *
+    * Database Vars
+    *
+    */
+    define("DBHOST", "localhost");
+    define("DB", "barman-db");
+    define("DBUSER", "root");
+    define("DBPW", "");
+
+    /*
+    *
+    * GPIO Vars
+    *
+    */
+    define("GPIO-FILEPATH", "C:\TEST\BarMan-gpio.py");
+
+    define("__ROOT__", dirname(__FILE__));
+
+    define("DEBUG", 0);
+?>
